048ecc549536dd6b5a400dd11a024b195e463057
Mahfuz Ali <mahfuz.ali@hitachiconsulting.com>
2020-04-29 12:41:54 +0100

    initial commit

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+def welcome(request):
+    return HttpResponse("Welcome to the Meeting Planner!")
+
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("I'm Reindert and I make courses for Pluralsight.")
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\138035\source\repos\GitHub\django_meeting_scheduler\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
